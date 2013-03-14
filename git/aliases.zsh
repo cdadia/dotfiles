3db6d990fd48797e52d58fc435d79e4a323f7bc7
Chirag Dadia <cdadia@gmail.com>
2013-03-14 01:01:52 -0300

    Update aliases.zsh

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,6 +7,8 @@ then
 fi
 
 # The rest of my fun git aliases
+alias g=git
+alias gup='git pull --rebase'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -17,3 +19,4 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
