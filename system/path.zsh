ac6d096c832a5f99c67d2806acf47019747f1a87
cdadia <cdadia@gmail.com>
2014-03-10 21:55:45 -0400

    Adding JAVA config

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,7 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
+
+export GROOVY_HOME="/usr/local/opt/groovy/libexec"
+
+export JAVA_HOME=$(/usr/libexec/java_home)
