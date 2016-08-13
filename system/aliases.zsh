824795504aeb308eb8e86c30626033fcf2e7dd43
cdadia <cdadia@gmail.com>
2016-08-13 14:51:58 -0400

    More additions

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -71,9 +71,15 @@ alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && k
 # System volume stuff
 alias mute="osascript -e 'set volume output muted true'"
 alias unmute="osascript -e 'set volume output muted false'"
-#alias dc="docker-compose "
-#alias dm="docker-machine "
-#alias doc="docker "
-#alias dwt="eval \"'$(docker-machine env dev)' ; docker exec -it '$(docker-compose ps -q web | sed -n 1p)' /bin/bash -c \". perpay/bin/activate; cd /webapps/perpay/perpay; coverage run --source='.' manage.py test; coverage report\"; "
-#pp-re() { dc stop "$@"; dc rm "$@" ; docker rmi icg/"$@":1.0.0 icg/"$@" ; make -C $@ release ; }
-#pp-make() { make -C /$@; }
+alias dc="docker-compose "
+alias dm="docker-machine "
+alias doc="docker "
+alias containers="doc ps -a"
+alias images="doc images"
+alias rmimg="doc rmi "
+alias doclogs="doc logs -f --tail=500 "
+alias dmc="dm create --virtualbox-disk-size '30000' -d virtualbox "
+alias dmrm="dm rm "
+
+# Vim
+alias vim='/usr/local/Cellar/vim/7.4.1817/bin/vim'
