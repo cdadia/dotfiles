e806aa1271e11b430190b36d52dd53c2f5fd545b
Chirag Dadia <chirag_dadia@cable.comcast.com>
2019-05-12 11:01:11 -0400

    Updates for latest version

--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,11 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/go/libexec/bin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 
 export GROOVY_HOME="/usr/local/opt/groovy/libexec"
 
 export JAVA_HOME=$(/usr/libexec/java_home)
+
+export PATH=$PATH:/usr/local/opt/go/libexec/bin
+
+export GOPATH=$HOME/go/
