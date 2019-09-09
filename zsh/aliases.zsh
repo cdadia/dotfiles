9deaf767615f249861a3cfc9dfaa32e1a410b6f3
Chirag Dadia <cdadia@urbn.com>
2019-09-09 08:23:21 -0400

    Update to aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,12 @@ alias reload!='. ~/.zshrc'
 alias k='kubectl'
 alias gcl='gcloud'
 alias fat='find . -type f -name "application-default.yaml" -exec yq r {} "kafka.topics" \; | grep name: | tr -s " " | cut -d " " -f 3 | sort -u | nl'
-#alias dockercleancontainers="docker ps -a -notrunc| grep 'Exit' | awk '{print \$1}' | xargs -L 1 -r docker rm"
-#alias dockercleanimages="docker images -a -notrunc | grep none | awk '{print \$3}' | xargs -L 1 -r docker rmi"
-#alias dockerclean="dockercleancontainers && dockercleanimages"
+alias dev='gcloud beta container clusters get-credentials rental-dev-v1 --region us-east4 --project rental-dev'
+alias stage='gcloud beta container clusters get-credentials rental-staging-v1 --region us-east4 --project rental-staging'
+alias prod='gcloud beta container clusters get-credentials rental-prod-v1 --region us-east4 --project rental-prod'
+alias dcc="docker ps -a | grep 'Exit' | awk '{print 1}' | xargs -L 1 docker rm"
+alias dci="docker images -a | grep none | awk '{print 3}' | xargs -L 1 docker rmi"
+alias dcl="dockercleancontainers && dockercleanimages"
+function gpf(){
+  kubectl -n $1 port-forward $2 $3
+}
